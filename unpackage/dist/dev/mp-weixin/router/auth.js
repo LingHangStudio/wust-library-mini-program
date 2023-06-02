17f85821950d35da5cb168a542226d35fc646e20
wustermarket <3198662089@qq.com>
2023-06-03 00:46:37 +0800

    unpackage

--- unpackage/dist/dev/mp-weixin/router/auth.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const common_vendor = require("../common/vendor.js");
-const authorizationKey = "user";
-function getAuthorization() {
-  return common_vendor.index.getStorageSync(authorizationKey);
-}
-exports.getAuthorization = getAuthorization;
